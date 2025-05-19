891bd782443f28bc5e559140b09bd8bd85af4bde
Rojin4455 <rojinsaji35@gmail.com>
2025-05-19 20:08:11 +0530

    added ip in allowed hosts

--- kpi_backend/settings.py
+++ kpi_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['16.171.40.146','localhost','127.0.0.1']
 
 
 # Application definition
